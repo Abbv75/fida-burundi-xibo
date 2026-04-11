b56f211353cbd601c8a697b0464dc7af0339fd6a
abbv75 <bore.younous59@gmail.com>
2026-04-11 20:15:05 +0000

    refactor: remove unused helper functions and associated PPM data types

--- src/helpers/formatToPercent.ts
+++
@@ -1,5 +0,0 @@
-export default (params: number) => params.toLocaleString("fr-FR", {
-    style: "percent",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-});
--- src/helpers/formatToXOF.ts
+++
@@ -1,1 +0,0 @@
-export default (params: number) =>`${params.toLocaleString()} CFA`
