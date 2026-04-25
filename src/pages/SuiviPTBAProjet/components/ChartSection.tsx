5a1eac9b35bd387b95d617a0984fe29afcfb40d3
abbv75 <bore.younous59@gmail.com>
2026-04-25 23:29:29 +0000

    jk

--- src/pages/SuiviPTBAProjet/components/ChartSection.tsx
+++ src/pages/SuiviPTBAProjet/components/ChartSection.tsx
@@ -59,7 +59,11 @@ export const ChartSection: React.FC<ChartSectionProps> = ({ dataMetrics }) => {
 
     return (
         <Box sx={{ width: '100%', height: '100%', minHeight: '400px' }}>
-            <HighchartsReact highcharts={Highcharts} options={chartOptions} containerProps={{ style: { height: '100%', width: '100%' } }} />
+            <HighchartsReact 
+                highcharts={Highcharts} 
+                options={chartOptions} 
+                containerProps={{ style: { height: '100%', width: '100%' } }} 
+            />
         </Box>
     );
 };
